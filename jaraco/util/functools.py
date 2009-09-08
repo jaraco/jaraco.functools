afb4a7df53a3c8d6fd196e3c6f5c0c4734869667
jaraco <none@none>
2009-09-08 21:43:28 +0000

    Added method_caller

--- jaraco/util/functools.py
+++ jaraco/util/functools.py
@@ -10,3 +10,19 @@ def compose(*funcs):
 	
 	compose_two = lambda f1, f2: lambda v: f1(f2(v))
 	return reduce(compose_two, funcs)
+
+def method_caller(method_name, *args, **kwargs):
+	"""
+	Return a function that will call a named method on the
+	target object with optional positional and keyword
+	arguments.
+	
+	>>> lower = method_caller('lower')
+	>>> lower('MyString')
+	'mystring'
+	"""
+	def call_method(target):
+		func = getattr(target, method_name)
+		return func(*args, **kwargs)
+	return call_method
+
