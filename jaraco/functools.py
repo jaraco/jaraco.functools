72bd570b1195125af3cfc3d03d1c6d3d5dc0760c
Jason R. Coombs <jaraco@jaraco.com>
2015-07-01 13:00:37 -0400

    Extract wait call

--- jaraco/functools.py
+++ jaraco/functools.py
@@ -173,9 +173,12 @@ class Throttler(object):
 		self.last_called = 0
 
 	def __call__(self, *args, **kwargs):
-		# ensure at least 1/max_rate seconds from last call
+		self._wait()
+		return self.func(*args, **kwargs)
+
+	def _wait(self):
+		"ensure at least 1/max_rate seconds from last call"
 		elapsed = time.time() - self.last_called
 		must_wait = 1 / self.max_rate - elapsed
 		time.sleep(max(0, must_wait))
 		self.last_called = time.time()
-		return self.func(*args, **kwargs)
